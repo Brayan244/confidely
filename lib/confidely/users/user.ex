a69d833eb3fb11859168986009e67f92d0bcc54a
Brayan Zavala <zavalabrayan244@gmail.com>
2024-06-14 17:15:06 -0600

    Adds several extensions to users

--- lib/confidely/users/user.ex
+++ lib/confidely/users/user.ex
@@ -1,4 +1,8 @@
 defmodule Confidely.Users.User do
+  @moduledoc """
+  The schema for users table, it represents both patients and therapists trhough the roles.
+  """
+
   use Ecto.Schema
   use Pow.Ecto.Schema
 
